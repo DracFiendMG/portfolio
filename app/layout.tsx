da547da7b088384f77d41daee07383ac4ac12604
DracFiendMG <sreeramrdy@gmail.com>
2026-08-02 22:11:29 +0530

    Enhance Home component: add card details for expertise and integrate Material Symbols font

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,10 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} ${jetBrainsMono.variable} ${sora.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&amp;display=swap" rel="stylesheet"></link>
+      </head>
+      <body className="min-h-full flex flex-col max-w-360 mx-auto">{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,22 @@ export default function Home() {
 
   // Section 2: Core Expertise
   const section2Title = "Core Expertise"
+  const cardDetails = [
+    {
+      icon: "payments",
+      title: "Fintech Systems",
+      description: "Building resilient, low-latency transaction engines and complex ledger systems using Spring Boot and Java."
+    },
+    {
+      icon: "psychology",
+      title: "AI-Accelerated",
+      description: "Leveraging GenAI for faster development cycles and integrating LLMs into enterprise-grade applications."
+    },
+    {
+      icon: "architecture",
+      title: "Tech Stack",
+    }
+  ]
 
   // Section 3: Selected Work
   const section3Title = "Selected Work"
@@ -28,7 +44,7 @@ export default function Home() {
   return (
     <>
       <div>
-        <div className="flex gap-10 min-h-160 justify-center">
+        <div className="flex gap-10 min-h-160 justify-between m-10">
           <div className="flex flex-col gap-4 relative">
             <p className="uppercase font-mono text-[12px] text-[#535353] tracking-[0.16em]">{roles.join(" / ")}</p>
             <div className="max-w-175">
@@ -45,18 +61,24 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div>
-        <h2>{section2Title}</h2>
-        <div>
-          <div>
-            Card1
-          </div>
-          <div>
-            Card2
-          </div>
-          <div>
-            Card3
-          </div>
+      <div className="m-10">
+        <h2 className="uppercase font-sora text-5xl font-extrabold">{section2Title}</h2>
+        <div className="border-4 w-33 mt-5 mb-10"></div>
+        <div className="grid grid-cols-3 gap-5">
+          {
+            cardDetails.map(card => {
+              return (
+                <div 
+                  key={card.icon} 
+                  className="flex flex-col gap-2 border-3 border-black min-w-75 card-shadow px-8 py-10 text-black even:bg-black even:text-[#00F0FF]"
+                >
+                  <span className="material-symbols-outlined text-4xl! mb-6">{card.icon}</span>
+                  <h3 className="font-bold font-inter">{card.title}</h3>
+                  <p className="font-light font-inter">{card.description}</p>
+                </div>
+              )
+            })
+          }
         </div>
       </div>
       <div>
