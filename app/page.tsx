ff5b5e8b693da402445ce19fc0819148959c4b9a
DracFiendMG <sreeramrdy@gmail.com>
2026-09-06 23:50:01 +0530

    Enhance Core Expertise section layout: add grid lines and improve styling for better visual appeal

--- app/page.tsx
+++ app/page.tsx
@@ -62,37 +62,39 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="mx-5 my-10 md:m-10 max-w-360">
-        <h2 className="uppercase font-sora text-3xl md:text-5xl font-extrabold">{section2Title}</h2>
-        <div className="border-2 md:border-4 w-[33%] md:w-33 mt-3 md:mt-5 mb-8 md:mb-10"></div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-          {
-            cardDetails.map(card => {
-              return (
-                <div 
-                  key={card.icon} 
-                  className="flex flex-col gap-2 border-3 border-black min-w-75 card-shadow px-8 py-10 text-black even:bg-black even:[&>h3,&>span]:text-[#00F0FF]"
-                >
-                  <span className="material-symbols-outlined text-3xl! md:text-4xl! md:mb-6">{card.icon}</span>
-                  <h3 className="uppercase font-medium font-sora text-xl md:text-2xl">{card.title}</h3>
-                  <p className="font-light font-sora text-[#737373]">{card.description}</p>
-                  {
-                    card.techstack && (
-                      <div className="flex flex-wrap gap-2 font-inter">
-                        {
-                          card.techstack.map(tech => {
-                            return (
-                              <span key={tech} className="border-2 px-2 py-1">{tech}</span>
-                            )
-                          })
-                        }
-                      </div>
-                    )
-                  }
-                </div>
-              )
-            })
-          }
+      <section className="max-w-360 grid-lines bg-gray-100">
+        <div className="mx-5 my-10 md:m-10">
+          <h2 className="uppercase font-sora text-3xl md:text-5xl font-extrabold">{section2Title}</h2>
+          <div className="border-2 md:border-4 w-[33%] md:w-33 mt-3 md:mt-5 mb-8 md:mb-10"></div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+            {
+              cardDetails.map(card => {
+                return (
+                  <div 
+                    key={card.icon} 
+                    className="flex flex-col gap-2 border-3 border-black min-w-75 card-shadow px-8 py-10 text-black even:bg-black odd:bg-white even:[&>h3,&>span]:text-[#00F0FF]"
+                  >
+                    <span className="material-symbols-outlined text-3xl! md:text-4xl! md:mb-6">{card.icon}</span>
+                    <h3 className="uppercase font-medium font-sora text-xl md:text-2xl">{card.title}</h3>
+                    <p className="font-light font-sora text-[#737373]">{card.description}</p>
+                    {
+                      card.techstack && (
+                        <div className="flex flex-wrap gap-2 font-inter">
+                          {
+                            card.techstack.map(tech => {
+                              return (
+                                <span key={tech} className="border-2 px-2 py-1">{tech}</span>
+                              )
+                            })
+                          }
+                        </div>
+                      )
+                    }
+                  </div>
+                )
+              })
+            }
+          </div>
         </div>
       </section>
       <section className="mx-5 my-10 md:m-10 flex flex-col gap-5 max-w-360">
@@ -118,12 +120,12 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="bg-black md:w-full flex justify-center">
+      <section className="bg-black md:w-full flex justify-center grid-lines">
         <div className="p-5 md:p-10 flex flex-col gap-10 max-w-360 h-100 justify-center">
           <h2 className="uppercase font-sora text-3xl md:text-5xl font-extrabold text-white max-w-240 text-center">Ready to engineer your next <span className="text-[#00F0FF]">high-performance system?</span></h2>
           <div className="flex flex-col md:flex-row gap-5 md:gap-10 items-center justify-center">
-            <button className="uppercase text-sm bg-white px-10 py-4 font-inter font-semibold w-60">{exploreWorkButtonText}</button>
-            <button className="uppercase text-sm bg-white px-10 py-4 font-inter font-semibold w-60">{downloadResumeButtonText}</button>
+            <button className="uppercase text-sm bg-white px-10 py-4 font-mono font-semibold w-60 hover:shadow-[6px_8px_0_0_#00F0FF] duration-100">{exploreWorkButtonText}</button>
+            <button className="uppercase text-sm bg-white px-10 py-4 font-mono font-semibold w-60">{downloadResumeButtonText}</button>
           </div>
         </div>
       </section>
