caabdfa379b1aea1346795077326b7710951a1a0
DracFiendMG <sreeramrdy@gmail.com>
2026-09-22 12:27:30 +0530

    feat: add resume page with layout and placeholder sections

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,13 +1,13 @@
 export default function Resume() {
     return (
         <div className="mx-auto w-[calc(100%-2.5rem)] md:w-[calc(100%-5rem)] max-w-360 flex flex-col gap-6">
-            <section className="flex flex-col md:flex-row gap-5">
-                <div className="flex flex-col gap-4">
+            <section className="grid grid-cols-1 md:grid-cols-12 gap-6">
+                <div className="md:col-span-7 flex flex-col gap-4">
                     <p className="uppercase text-[#00929b] font-mono text-xs md:tracking-wider">Current Status: Available for work</p>
                     <h1 className="uppercase text-5xl md:text-7xl font-sora font-bold tracking-tight">Resume</h1>
                     <p className="text-[#737373] font-inter">Senior Software Engineer / Full Stack Developer with 3+ years of experience in fintech and enterprise environments (Java/Spring Boot, Angular). Full-stack expert focused on AI tooling and high-impact solutions.</p>
                 </div>
-                <div>
+                <div className="md:self-end md:justify-self-end md:col-span-5">
                     <button className="
                         bg-black text-white px-10 py-4 flex gap-4 shadow-[4px_4px_0_0_#000000] uppercase text-xs items-center font-mono
                         hover:shadow-[8px_8px_0_0_#00F0FF] hover:transition-all duration-300 hover:-translate-x-1 hover:-translate-y-1 cursor-pointer
@@ -20,7 +20,12 @@ export default function Resume() {
                 </div>
             </section>
             <section>
-
+                <div>
+                    Work Experience
+                </div>
+                <div>
+                    Education
+                </div>
             </section>
             <section>
 
