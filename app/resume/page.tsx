504be3e9432a71becd9e17774f0e09ad1b02515d
DracFiendMG <sreeramrdy@gmail.com>
2026-09-22 11:43:48 +0530

    feat: add resume page component with download button

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,33 @@
 export default function Resume() {
     return (
-        <div>
-            Resume
+        <div className="mx-auto w-[calc(100%-2.5rem)] md:w-[calc(100%-5rem)] max-w-360 flex flex-col gap-6">
+            <section className="flex flex-col md:flex-row gap-5">
+                <div className="flex flex-col gap-4">
+                    <p className="uppercase text-[#00929b] font-mono text-xs md:tracking-wider">Current Status: Available for work</p>
+                    <h1 className="uppercase text-5xl md:text-7xl font-sora font-bold tracking-tight">Resume</h1>
+                    <p className="text-[#737373] font-inter">Senior Software Engineer / Full Stack Developer with 3+ years of experience in fintech and enterprise environments (Java/Spring Boot, Angular). Full-stack expert focused on AI tooling and high-impact solutions.</p>
+                </div>
+                <div>
+                    <button className="
+                        bg-black text-white px-10 py-4 flex gap-4 shadow-[4px_4px_0_0_#000000] uppercase text-xs items-center font-mono
+                        hover:shadow-[8px_8px_0_0_#00F0FF] hover:transition-all duration-300 hover:-translate-x-1 hover:-translate-y-1 cursor-pointer
+                        ">
+                        Download PDF
+                        <span className="material-symbols-outlined text-lg!">
+                            download
+                        </span>
+                    </button>
+                </div>
+            </section>
+            <section>
+
+            </section>
+            <section>
+
+            </section>
+            <section>
+
+            </section>
         </div>
     )
 }
